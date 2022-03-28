571a897076f9ff2a70a617020acd74001885677d
Mateusz Chilinski <mateusz@chilinski.eu>
2022-03-28 17:51:28 +0200

    zendo release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Mateusz Chiliński, Dariusz Plewczyński'
 author = 'Mateusz Chiliński, Dariusz Plewczyński'
 
 # The full version, including alpha/beta/rc tags
-release = '1.8'
+release = '1.9'
 
 
 # -- General configuration ---------------------------------------------------
